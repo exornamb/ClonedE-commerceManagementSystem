3a5e3e49a338b94fdff56d9fe0f31ab811934f97
exornamb <exornamb@gmail.com>
2025-05-29 12:19:16 +0000

    getting product by id

--- src/main/java/com/codewithmosh/store/controller/ProductController.java
+++ src/main/java/com/codewithmosh/store/controller/ProductController.java
@@ -4,10 +4,8 @@ import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.mapper.ProductMapper;
 import com.codewithmosh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,15 @@ public class ProductController {
                 .stream()
                 .map(productMapper::toProductDto)
                 .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long id){
+        var product = productRepository.findById(id).orElse(null);
+        if(product == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(productMapper.toProductDto(product));
     }
 }
